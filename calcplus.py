cf2581c41e1776cbef7cf5c94ec4c462b63c0287
victor4correa <v.correar@alumnos.urjc.es>
2018-10-01 14:34:27 +0200

    Calcplus Pep8 superado y funcionando

---
+++ calcplus.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  1 09:34:36 2018
+
+@author: cvillar
+"""
+
+import sys
+
+from calcoohija import calculadoraHija
+if __name__ == "__main__":
+    calc = calculadoraHija()
+    fich_calcu = open(sys.argv[1], 'r')
+    Lista = fich_calcu.readlines()
+    fich_calcu.close()
+
+    for line in Lista:
+        operacion = True
+        num = line.split(',')
+        num[-1] = num[-1][:-1]
+        operans = num[1:]
+
+        if num[0] == "suma":
+            op_inic = 0
+            for operando in operans:
+                op_inic = calc.plus(op_inic, int(operando))
+
+            print("El resultado de la suma es:", op_inic)
+
+        elif num[0] == "resta":
+
+            op_inic = int(operans[0])
+            for operando in operans[1:]:
+                op_inic = calc.minus(op_inic, int(operando))
+
+            print("El resultado de la resta es:", op_inic)
+
+        elif num[0] == "multiplica":
+            op_inic = 1
+            for operando in operans:
+                op_inic = calc.multi(op_inic, int(operando))
+
+            print("El resultado de la multiplicacion es:", op_inic)
+
+        elif num[0] == "divide":
+            op_inic = int(operans[0])
+
+            try:
+                    for operando in operans[1:]:
+                        op_inic = calc.division(op_inic, int(operando))
+                    print("El resultado de la division es:", op_inic)
+
+            except ZeroDivisionError:
+                sys.exit("Error: Division by zero is not allowed")
+
+        else:
+            operacion = False
+            print ("Operación no valida")
